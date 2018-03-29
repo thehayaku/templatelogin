f5b61fef54c108fb7c9ca5d9745408866a373b46
thehayaku <36600471+thehayaku@users.noreply.github.com>
2018-03-29 20:17:16 +0700

    Add files via upload

---
+++ temanParty/index.php
@@ -0,0 +1,31 @@
+<html>
+<head>
+	<title>form login</title>
+	<meta name='viewport' content='width=device-width, initial-scale=1'>
+	<link href='css/style.css' rel='stylesheet' type='text/css'>
+	<link href="https://fonts.googleapis.com/css?family=Mukta+Mahee" rel="stylesheet">
+</head>
+<body>
+  <div class='header'></div>
+  <div class='content'>
+    <div id='title'>
+    </br>
+    <h1>MEET YOUR PARTY</h1>
+    </div>
+    <form action="#" method="post">
+	<div id='picuna'><img src='images/username.png' width='40' height='55' align='middle'></div>
+	<div id='una'>Username</div>
+	<div id='inuna'><input type="text" name="name" required size='30'/></div>
+	<div id='picpas'><img src='images/kry.png' width='30' height='66' align='bottom'></div>
+	<div id='pas'>Password</div>
+	<div id='inpas'><input type="text" name="password" required size='30'/></div>
+	<div id='login'><input type="button" id='loginbtn' value="LOGIN" /></div>
+	</form>
+  
+</div>
+<div class='footer'>
+<p>designed by theHayaku</p>
+</div>
+
+</body>
+</html>
